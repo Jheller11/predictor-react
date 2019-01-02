49bab17be781ae8faa318e6dc358c07036e2ddd3
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-01-02 14:00:54 -0500

    research and implement CSS Modules

---
+++ resources.md
@@ -0,0 +1,7 @@
+1. Using CSS Modules with Create React App:
+
+- https://facebook.github.io/create-react-app/docs/adding-a-css-modules-stylesheet
+
+2. Authentication with React/Express/JSON Web Tokens
+
+- https://medium.com/@faizanv/authentication-for-your-react-and-express-application-w-json-web-tokens-923515826e0
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Nav.module.css'
 
 const Nav = loggedIn => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Log In</Link>
-      <Link to="/signup">Sign Up</Link>
+    <nav className={styles['main-nav']}>
+      <Link className={styles['link']} to="/">
+        Premier League Predictor
+      </Link>
+      <Link className={styles['link']} to="/about">
+        About
+      </Link>
+      <Link className={styles['link']} to="/login">
+        Log In
+      </Link>
+      <Link className={styles['link']} to="/signup">
+        Sign Up
+      </Link>
     </nav>
   )
 }
