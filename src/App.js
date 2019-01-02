6bab71bc9618f41a5b8ad55985a60cf92977dc88
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-01-02 12:41:04 -0500

    config react-router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import Nav from './components/Nav/Nav'
 import About from './components/About/About'
 import Signup from './components/Signup/Signup'
+import Login from './components/Login/Login'
+import Home from './components/Home/Home'
 import './App.css'
 
 class App extends Component {
